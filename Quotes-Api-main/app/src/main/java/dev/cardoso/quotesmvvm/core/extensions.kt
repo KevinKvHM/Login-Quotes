96c48f6606dd0db39c3e65bfc73f61110e997aea
KevinKevHM <85089531+KevinKevHM@users.noreply.github.com>
2021-11-23 18:54:40 -0600

    view-Login

---
+++ Quotes-Api-main/app/src/main/java/dev/cardoso/quotesmvvm/core/extensions.kt
@@ -0,0 +1,33 @@
+package dev.cardoso.quotesmvvm.core
+
+import dev.cardoso.quotesmvvm.data.local.entities.QuoteEntity
+import dev.cardoso.quotesmvvm.data.model.LoginResponse
+import dev.cardoso.quotesmvvm.data.model.QuoteModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
+
+
+
+suspend fun <T> Flow<List<T>>.convertToList() =
+    flatMapConcat { it.asFlow() }.toList()
+
+fun Flow<QuoteEntity>.toQuoteModel() : Flow<QuoteModel> {
+   return this.map {
+       QuoteModel(id= it.id,
+           quote = it.quote,
+           author = it.author)
+   }
+}
+
+fun  List<QuoteModel>.toQuoteEntity(): List<QuoteEntity> {
+    return this.map {
+        QuoteEntity(id= it.id,
+            quote = it.quote,
+            author = it.author)
+    }
+}
+
+fun LoginResponse.isSuccessful(): Boolean {
+    return this.success
+}
