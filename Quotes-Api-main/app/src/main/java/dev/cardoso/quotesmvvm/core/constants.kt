d137537b284f7288673315e0617714b9e14a4537
KevinKevHM <85089531+KevinKevHM@users.noreply.github.com>
2021-11-23 19:04:53 -0600

    Login

--- Quotes-Api-main/app/src/main/java/dev/cardoso/quotesmvvm/core/constants.kt
+++ Quotes-Api-main/app/src/main/java/dev/cardoso/quotesmvvm/core/constants.kt
@@ -2,6 +2,6 @@ package dev.cardoso.quotesmvvm.core
 
 
 //const val BASE_URL = "https://raw.githubusercontent.com/jomc-hash/mvc-usuarios/master/"
-const val BASE_URL = "http://192.168.1.65:8080"
+const val BASE_URL = "http://192.168.1.115:8080"
 const val API_PATH="quotes.php"
 const val LOGIN_URL= "/api/v1/users/login"
